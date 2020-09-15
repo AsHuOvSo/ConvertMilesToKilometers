7d4c02912c319aa9f69f335183f228fabef18948
AsHuOvSo <asta@avra.dk>
2020-09-15 11:34:18 +0200

    Written main code. Finished excersize.

--- src/ConvertMilesToKm.java
+++ src/ConvertMilesToKm.java
@@ -1,5 +1,18 @@
+//Import Scanner library
+import java.util.Scanner;
+
 public class ConvertMilesToKm {
+    //Start of main code
     public static void main(String[] args) {
-        
+        //Printing out start message
+        System.out.println("Please enter miles:");
+        //Scan for input
+        Scanner milesInput = new Scanner(System.in);
+        //Convert scanned data to datatype double
+        double miles = milesInput.nextDouble();
+        //Math
+        double kilometers = miles * 1.6;
+        //print result
+        System.out.println(miles + " miles is " + kilometers + " kilometers.");
     }
 }
